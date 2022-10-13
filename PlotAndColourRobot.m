b1f7679fb377a38abd86ff2cfe5b4adfc2c6d3f2
chanpe207 <chanpe207@gmail.com>
2022-10-13 16:29:50 +1100

    Auboi3 and Plot and Colour function files

---
+++ PlotAndColourRobot.m
@@ -0,0 +1,30 @@
+function PlotAndColourRobot(r, workspace)
+    for linkIndex = 0:r.n
+        [ faceData, vertexData, plyData{linkIndex+1} ] = plyread(['LinUR3Link',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>
+
+        r.faces{linkIndex+1} = faceData;
+        r.points{linkIndex+1} = vertexData;
+    end
+
+    % Display robot
+    r.plot3d(zeros(1,r.n),'noarrow','workspace',workspace);
+    if isempty(findobj(get(gca,'Children'),'Type','Light'))
+        camlight
+    end  
+    r.delay = 0;
+
+    % Try to correctly colour the arm (if colours are in ply file data)
+    for linkIndex = 0:r.n
+        handles = findobj('Tag', r.name);
+        h = get(handles,'UserData');
+        try 
+            h.link(linkIndex+1).Children.FaceVertexCData = [plyData{linkIndex+1}.vertex.red ...
+                                                          , plyData{linkIndex+1}.vertex.green ...
+                                                          , plyData{linkIndex+1}.vertex.blue]/255;
+            h.link(linkIndex+1).Children.FaceColor = 'interp';
+        catch ME_1
+            disp(ME_1);
+            continue;
+        end
+    end
+end
