38cc807de44675df516830aaad674126fa7b3822
chanpe207 <chanpe207@gmail.com>
2022-10-15 13:21:52 +1100

    Create SprayPaintUR3.m

---
+++ SprayPaintUR3.m
@@ -0,0 +1,28 @@
+function SprayPaintUR3()
+%SPRAYPAINTUR3 Summary of this function goes here
+
+%   create the UR3 at a point in the workspace
+r = UR3;
+% create variable for joints
+q = zeros(1,r.model.n);
+
+%% Move from zero position to position ready to spray
+
+readyEEPosition = [-0.2; -0.2; 0.42];
+readyEEAngles = [0 -pi/2 pi/2];
+readyEEOrientation = eul2rotm(readyEEAngles);
+steps = 20;
+q1 = r.model.getpos();
+T2 = cat(1,cat(2,readyEEOrientation,readyEEPosition),[0 0 0 1]);
+q2 = r.model.ikcon(T2);
+
+qMatrix = jtraj(q1,q2,steps);
+
+for i = 1:steps
+    r.model.animate(qMatrix(i,:));
+    pause();
+end
+
+%% Translate paper corners away from paper by x distance
+end
+
