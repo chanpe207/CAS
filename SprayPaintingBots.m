3d3204ba8215442b2be7094e47bcb7c8ee2eca24
chanpe207 <chanpe207@gmail.com>
2022-10-15 13:34:04 +1100

    Create SprayPaintingBots.m

---
+++ SprayPaintingBots.m
@@ -0,0 +1,23 @@
+classdef SprayPaintingBots
+    %SPRAYPAINTINGBOTS Summary of this class goes here
+    %   A dual cobot system designed to spray paint pieces of paper
+    
+    properties
+        Property1
+    end
+    
+    methods
+        function obj = SprayPaintingBots(inputArg1,inputArg2)
+            %SPRAYPAINTINGBOTS Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
+
